b1b448f6c568e4481dac680152c99b42199ee973
Reda achouih <accnumber4441@gmail.com>
2026-05-26 18:30:59 +0100

    redesign: zen-inspired landing page with browser mockup

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.download-card').forEach(card => {
-  card.addEventListener('click', e => {
-    e.preventDefault()
-    window.open('https://github.com/accnumber4441-del/itran-browser/releases', '_blank')
-  })
-})
